2ddeec90f60de7de20ccb941d046928e959c0d42
saidabdul80 <saidabdulsalam5@gmail.com>
2023-05-16 05:13:28 +0100

    fix: updates

--- routes/api.php
+++ routes/api.php
@@ -39,9 +39,9 @@ Route::put('/products/{id}', [ProductController::class, 'update']);
 Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 
 // Orders
-Route::get('/orders', [OrderController::class, 'index']);
+//Route::get('/orders', [OrderController::class, 'index']);
 Route::get('/orders/{id}', [OrderController::class, 'show']);
-Route::post('/orders', [OrderController::class, 'store']);
+Route::any('/orders', [OrderController::class, 'store']);
 Route::get('/ussd_orders', [OrderController::class, 'store']);
 Route::put('/orders/{id}', [OrderController::class, 'update']);
 Route::delete('/orders/{id}', [OrderController::class, 'destroy']);
